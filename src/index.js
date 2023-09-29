dcd6f8ef4b17d39abbeeea0b92447cb765acac58
Samuel Muli <samuelmuli95@gmail.com>
2023-09-29 18:13:49 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  //Get the form element
+  const form = document.querySelector('#create-task-form')
+  //Get the task list element
+  const taskList = document.querySelector('#tasks')
+  //Add event listener to the form
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const taskDescription = document.querySelector('#new-task-description').value
+
+    const newTask = document.createElement('li')
+    newTask.innerText = taskDescription
+
+    const deleteButton = document.createElement('button')
+    deleteButton.innerText = `X`
+    deleteButton.addEventListener('click', removeTask)
+
+    newTask.appendChild(deleteButton)
+    taskList.appendChild(newTask)
+    form.reset()
+  })
+
+  function removeTask(e) {
+    e.target.parentElement.remove()
+  }
 });
+
+
